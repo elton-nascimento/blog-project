0bf3c69506a7556b4a26e6225e7f3fd789526eba
enascimento <eltonnjohnny@gmail.com>
2025-10-04 17:38:36 -0300

    feat: Correct

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-// next.config.ts
-
-import { NextStudio } from 'next-sanity/studio'
 import type { NextConfig } from 'next'
 
-const config: NextConfig = {
-  // A sua configuração do Next.js pode ter outras opções aqui.
-  // Deixe-as como estão.
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
-export default NextStudio(config)
+export default nextConfig
